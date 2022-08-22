80da2e0915ed180c72a033822e28ea7f9cd8c514
idan chen <idanos@idans-MacBook-Pro-68.local>
2022-08-22 11:13:06 +0300

    added list scanning feature for endpoint wordlist generator

--- endpointListCreator.py
+++ endpointListCreator.py
@@ -24,11 +24,11 @@ response_data = ""
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'}
 
 def start():
-    action = input("What would you like to do? \n\n 1- Generate endpoint wordlist \n 2- Crawl the application \n\n Your Choice: ")
+    action = input("What would you like to do? \n\n 1- Generate endpoint wordlist \n 2- Crawl the application (Work In Progress...) \n\n Your Choice: ")
     
     #action 1 - just generate an endpoint wordlist
     if action == "1":
-        scan_type = input("\n Choose Scan Type\n________________ \n\n 1- Single Target \n 2- Subdomain List \n\n Your Choice: ")
+        scan_type = input("\n Choose Scan Type\n________________ \n\n 1- Single Target \n 2- List \n\n Your Choice: ")
 
         #Single target scan, user enters a single URL
         if scan_type == "1":    
@@ -63,8 +63,43 @@ def start():
 
         #Subdomain list scan, user enters a path to subdomain list text file
         elif scan_type == "2":
-            print("\n\n Still In Development... \n\n")
-            start()
+            path = input("Enter Path To Save Output file: ")
+            counter = 0
+            targets = []
+            list_file = input("Enter full path to your list.txt: ")
+            lf = open(list_file, 'r')
+            for line in lf:
+                targets.append(line.strip()+"/robots.txt")
+                file_name_template = targets[counter].replace("https://","").strip("/robots.txt")
+                r = requests.get(targets[counter])
+                
+                with open(path+'/'+file_name_template+'_endpointWordlist.txt', 'w+') as file:
+                    file.write(r.text)
+                file.close()
+
+                f = open(path+'/'+file_name_template+'_endpointWordlist.txt', "r+")
+                new_file = []
+
+                for line in f:
+                    line = line.replace("*","")
+    
+                    if "Allow: " in line:
+                        only_endpoint = line.split("Allow: ")
+                        new_file.append(only_endpoint[1])
+
+                    elif "Disallow: " in line:
+                        only_endpoint = line.split("Disallow: ")
+                        new_file.append(only_endpoint[1])
+        
+        
+                with open(path+'/'+file_name_template+'_endpointWordlist.txt', "w+") as f:
+                    for i in new_file:
+                        f.write(i+"\n")
+                f.close()
+                counter = counter+1
+
+            #print("\n\n Still In Development... \n\n")
+           # start()
 
         else:
             print("\n Invalid Input, Please Choose A Number From The Menu \n")
